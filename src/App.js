9c75aa7a0cd3ac5ac5da85c7c24d846ea976ed14
Eugen Levandovskyi <zshenialev@gmail.com>
2024-04-13 22:36:33 +0300

    3

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+   <>
+   sdfsdf
+   </>
+
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import App from './App';
+import { ReactDOM } from 'react';
+import React from 'react';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
